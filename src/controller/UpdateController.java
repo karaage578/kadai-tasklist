dc70052864c597da23b6998de814728c082a4876
masanori.takeda <masanori.takeda@weserve.co.jp>
2019-10-02 15:46:23 +0900

    課題提出コミット

--- src/controller/UpdateController.java
+++ src/controller/UpdateController.java
@@ -2,8 +2,10 @@ package controller;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
 import utils.DBUtil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class UpdateController
@@ -45,18 +48,31 @@ public class UpdateController extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);
 
-            //データベースを更新
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
-            em.close();
+            //バリデーション実行
+            List<String> errors = MessageValidator.validate(t);
+            if (errors.size() > 0) {
 
-            //セッションスコープ上の不要になったデータの削除
-            request.getSession().removeAttribute("task_id");
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
 
-            //indexページへのリダイレクト
-            response.sendRedirect(request.getContextPath() + "/index");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+                rd.forward(request, response);
+            } else {
+
+                //データベースを更新
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                //セッションスコープ上の不要になったデータの削除
+                request.getSession().removeAttribute("task_id");
+
+                //indexページへのリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
-
 }
